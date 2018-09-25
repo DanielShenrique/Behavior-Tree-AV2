67f93cb9f847c59c135d90fe2c6fa5b1b3aff497
Aluno <aluno@cejoseleitelopes.com.br>
2018-09-25 15:17:27 -0300

    1
    1 - começo da AI

---
+++ Assets/Script/AI_Commands.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BTAI;
+
+public class AI_Commands : MonoBehaviour {
+
+    Root tree = BT.Root();
+
+    bool isVisible = false;
+
+    private void Start()
+    {
+        tree.OpenBranch(
+
+            BT.Sequence().OpenBranch(
+
+                BT.If(() => IsVisible()).OpenBranch(
+                    
+                    
+                    )
+
+                )
+
+            );
+    }
+
+    private void Update()
+    {
+        tree.Tick();
+    }
+
+    ///
+    // Fazer funções para a comparação dos "if"sda AI
+    ///
+
+    bool IsVisible() { return isVisible; }
+
+}
